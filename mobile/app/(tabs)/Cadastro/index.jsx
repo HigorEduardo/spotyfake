e09b42c2f5a6fae5d06b89eca761c966ab4c79c4
HIGOR EDUARDO DA SILVA <higor_e_silva@estudante.sesisenai.org.br>
2024-11-18 12:20:12 -0300

    18/11

--- mobile/app/(tabs)/Cadastro/index.jsx
+++ mobile/app/(tabs)/Cadastro/index.jsx
@@ -13,32 +13,32 @@ export default function App() {
             return alert('Todos os campos devem ser preenchidos');
         }
 
-        const formData = {name: name, bday: bday, email: email, password: password};
+        const formData = { name: name, bday: bday, email: email, password: password };
 
         try {
             const res = fetch("http://localhost:8000/registro", {
-              method: "POST",
-              headers: {
-                Accept: "application/json",
-                "Content-type": "application/json",
-              },
-              body: JSON.stringify(formData),
+                method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-type": "application/json",
+                },
+                body: JSON.stringify(formData),
             });
             switch (response.status) {
-              case 201:
-                alert("Usuário criado");
-                break;
-              case 406:
-                alert("Preencha todos os campos");
-                break;
-              case 418:
-                alert("Email já cadastrado");
-                break;
-              default:
-                alert("Erro ao se conectar com servidor");
-                break;
+                case 201:
+                    alert("Usuário criado");
+                    break;
+                case 406:
+                    alert("Preencha todos os campos");
+                    break;
+                case 418:
+                    alert("Email já cadastrado");
+                    break;
+                default:
+                    alert("Erro ao se conectar com servidor");
+                    break;
             }
-          } catch (error) {}
+        } catch (error) { }
     }
 
     return (
@@ -48,124 +48,127 @@ export default function App() {
                 <Text style={styles.headerTextup}>SpotyFake</Text>
                 <Text style={styles.subHeaderTextup}>Cadastre-se para ouvir sua música favorita!</Text>
             </View>
-
-            <View style={styles.formup}>
-                <TextInput
-                    style={styles.inputup}
-                    placeholder="Name"
-                    placeholderTextColor="#fff"
-                    value={name}
-                    onChangeText={(text) => setName(text)}
-                />
-                <TextInput
-                    style={styles.inputup}
-                    placeholder="E-mail"
-                    placeholderTextColor="#fff"
-                    keyboardType="email-address"
-                    value={email}
-                    onChangeText={(text) => setEmail(text)}
-                />
-                <TextInput
-                    style={styles.inputup}
-                    placeholder="dd/mm/aaaa"
-                    placeholderTextColor="#fff"
-                    value={bday}
-                    onChangeText={(text) => setBday(text)}
-                />
-                <TextInput
-                    style={styles.inputup}
-                    placeholder="Senha"
-                    placeholderTextColor="#fff"
-                    secureTextEntry
-                    value={password}
-                    onChangeText={(text) => setPassword(text)}
-                />
-                <Link href="/">
-                    <TouchableOpacity style={styles.buttonup} onPress={handleRegister}>
-                        <Text style={styles.buttonTextup}>Cadastre-se</Text>
-                    </TouchableOpacity>
-                </Link>
-
+          
+                <View style={styles.formup}>
+                    <TextInput
+                        style={styles.inputup}
+                        placeholder="Name"
+                        placeholderTextColor="#fff"
+                        value={name}
+                        onChangeText={(text) => setName(text)}
+                    />
+                    <TextInput
+                        style={styles.inputup}
+                        placeholder="E-mail"
+                        placeholderTextColor="#fff"
+                        keyboardType="email-address"
+                        value={email}
+                        onChangeText={(text) => setEmail(text)}
+                    />
+                    <TextInput
+                        style={styles.inputup}
+                        placeholder="dd/mm/aaaa"
+                        placeholderTextColor="#fff"
+                        value={bday}
+                        onChangeText={(text) => setBday(text)}
+                    />
+                    <TextInput
+                        style={styles.inputup}
+                        placeholder="Senha"
+                        placeholderTextColor="#fff"
+                        secureTextEntry
+                        value={password}
+                        onChangeText={(text) => setPassword(text)}
+                    />
+                    <Link href="/">
+                        <TouchableOpacity style={styles.buttonup} onPress={handleRegister}>
+                            <Text style={styles.buttonTextup}>Cadastre-se</Text>
+                        </TouchableOpacity>
+                    </Link>
+                </View>
                 <View style={styles.signupContainerup}>
                     <Text style={styles.signupTextup}>Já tem conta?</Text>
                     <Link href="/">
                         <Text style={styles.signupLinkup}>Entre</Text>
                     </Link>
                 </View>
-            </View>
         </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
-    containerup: {
-        flex: 1,
-        backgroundColor: 'black',
-        justifyContent: 'center',
-    },
-    headerup: {
-        alignItems: 'center',
-        marginBottom: 40,
-    },
-    headerTextup: {
-        marginTop: -50,
-        fontSize: 32,
-        fontWeight: 'bold',
-        color: '#a80000',
-    },
-    subHeaderTextup: {
-        fontSize: 16,
-        color: 'white',
-        marginTop: 10,
-    },
-    formup: {
-        paddingHorizontal: 20,
-    },
-    inputup: {
-        height: 50,
-        borderColor: '#ccc',
-        borderWidth: 1,
-        borderRadius: 10,
-        paddingHorizontal: 15,
-        marginBottom: 20,
-        fontSize: 16,
-        backgroundColor: 'grey',
-        color: 'white'
-    },
-    buttonup: {
-        backgroundColor: '#a80000',
-        height: 50,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 10,
-        marginLeft:650
-    },
-    buttonTextup: {
-        color: '#fff',
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-    signupContainerup: {
-        marginTop:-60,
-        flexDirection: 'row',
-        justifyContent: 'center',
-    },
-    signupTextup: {
-        fontSize: 16,
-        color: 'white',
-        marginRight: 5,
-    },
-    signupLinkup: {
-        color: '#a80000',
-        fontSize: 16,
-        fontWeight: 'bold',
-        marginLeft: 5,
-
-    },
-    imageup: {
-        marginTop: -20,
-        marginBottom: 50,
-        width: 200,
-        height: 200
-    }
+        containerup: {
+            flex: 1,
+            backgroundColor: 'black',
+            justifyContent: 'center',
+        },
+        headerup: {
+            alignItems: 'center',
+            marginBottom: 40,
+        },
+        headerTextup: {
+            marginTop: -50,
+            fontSize: 32,
+            fontWeight: 'bold',
+            color: '#a80000',
+        },
+        subHeaderTextup: {
+            fontSize: 16,
+            color: 'white',
+            marginTop: 10,
+            marginBottom:30
+        },
+        formup: {
+            marginTop: -40,
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        inputup: {
+            width: '90%',
+            height: 50,
+            borderColor: '#ccc',
+            borderWidth: 1,
+            borderRadius: 10,
+            paddingHorizontal: 15,
+            marginBottom: 20,
+            fontSize: 16,
+            backgroundColor: 'grey',
+            color: 'white',
+        },
+        buttonup: {
+            backgroundColor: '#a80000',
+            height: 40,
+            width: 120,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 10,
+        },
+        buttonTextup: {
+            color: '#fff',
+            fontSize: 18,
+            fontWeight: 'bold',
+        },
+        signupContainerup: {
+            flexDirection: 'row',
+            justifyContent: 'center',
+        },
+        signupTextup: {
+            marginBottom: 70,
+            fontSize: 16,
+            color: 'white',
+            marginRight: 5,
+        },
+        signupLinkup: {
+            color: '#a80000',
+            fontSize: 16,
+            fontWeight: 'bold',
+            marginLeft: 5,
+            marginBottom: -40
+        },
+        imageup: {
+            marginBottom: 50,
+            width: 150,
+            height: 150
+        }
 });
